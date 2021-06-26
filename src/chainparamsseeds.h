a96ed90909ca834106e6c0b694724e41185f3cda
RANDOLPH <dr2370@att.com>
2021-06-26 14:40:06 -0500

    Dillycoin: Branding

--- src/chainparamsseeds.h
+++ src/chainparamsseeds.h
@@ -7,11 +7,8 @@
  * Each line contains a 16-byte IPv6 address and a port.
  * IPv4 as well as onion addresses are wrapped inside an IPv6 address accordingly.
  */
-static SeedSpec6 pnSeed6_main[] = {
-};
+static SeedSpec6 pnSeed6_main[] = {};
 
-static SeedSpec6 pnSeed6_test[] = {
-    
-};
+static SeedSpec6 pnSeed6_test[] = {};
 
 #endif // BITCOIN_CHAINPARAMSSEEDS_H
