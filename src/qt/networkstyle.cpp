1553596d6960a6cf90516bf113fb78c79e9fc3a3
RANDOLPH <dr2370@att.com>
2021-06-27 20:21:28 -0500

    Dillycoin: Branding

--- src/qt/networkstyle.cpp
+++ src/qt/networkstyle.cpp
@@ -29,7 +29,7 @@ NetworkStyle::NetworkStyle(const QString &_appName, const int iconColorHueShift,
     // load pixmap
     QPixmap pixmap;
     if (std::char_traits<char>::length(_titleAddText) == 0) {
-        pixmap.load(":/icons/bitcoin");
+        pixmap.load(":/icons/dillycoin");
     } else {
         pixmap.load(":/icons/dillycoin_splash");
     }
