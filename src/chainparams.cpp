597017d6a09dfd49612791f0fb8fa7b7f3a66616
Don Randolph <dr2370@att.com>
2021-06-29 15:15:08 -0500

    Dillycoin: Branding

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -297,8 +297,8 @@ public:
 
         genesis = CreateGenesisBlock(1624990309, 383771, 0x1e0ffff0, 1, 50 * COIN);
         consensus.hashGenesisBlock = genesis.GetHash();
-        assert(consensus.hashGenesisBlock == uint256S("0xcc45cd3e333405429f4173741c2723d8cf5198513ad4b2a64b501d645163049f"));
-        assert(genesis.hashMerkleRoot == uint256S("0xaeed56cc6801a42deab35a1957a9103001051122bb1110b0533272b8b8fb52d8"));
+        assert(consensus.hashGenesisBlock == uint256S("0xfe893ed755ed9a8d1e340ea07253bf27dbe30e7630f0e388d55cbe4cd5150eea"));
+        assert(genesis.hashMerkleRoot == uint256S("0x2c77678bd3cb68c94fb49b7428fedc37e72f145b93bc4258ebd7e5e3a93759e1"));
 
         vFixedSeeds.clear(); //!< Regtest mode doesn't have any fixed seeds.
         vSeeds.clear();      //!< Regtest mode doesn't have any DNS seeds.
